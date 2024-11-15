a64b514907995a3743f469b8367881c2dad126c5
dawitzeleke <dawitzeleke92@gmail.com>
2024-11-15 18:59:09 +0300

    implement the backend

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+
+import HomePage from "./home/page";
+
+
+export default function Home() {
+  return (
+    <main>
+      <HomePage/>
+    </main>
+      
+  ); 
+}
